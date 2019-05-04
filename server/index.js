6ae35e85db6f9a8e6e4cf3c7c34954fd1a741b69
Francesco Commisso <francescocommissodev@gmail.com>
2019-05-04 13:55:05 -0400

    added heroku post-build script

--- server/index.js
+++ server/index.js
@@ -8,16 +8,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: false }));
 app.use(pino);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
 //production mode
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-//   //
-//   app.get("*", (req, res) => {
-//     res.sendfile(path.join((__dirname = "client/build/index.html")));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "../build/index.html")));
+  });
+}
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../public/index.html"));
+});
 
 var games = {};
 
@@ -41,6 +43,6 @@ app.post("/api/input/endturn", (req, res) => {
   res.send(JSON.stringify(games[req.body.id]));
 });
 
-app.listen(5000, () =>
-  console.log("Express server is running on localhost:5000")
+app.listen(3001, () =>
+  console.log("Express server is running on localhost:3001")
 );
